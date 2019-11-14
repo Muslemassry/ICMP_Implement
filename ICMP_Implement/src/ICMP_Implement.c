4f9fbcd2abe851d5341d56937263cead97a5b60f
root <root@mim-HP-ProBook-4540s>
2019-11-14 22:51:13 +0300

    -- to continue building the send echo icmp reqeusts

--- ICMP_Implement/src/ICMP_Implement.c
+++ ICMP_Implement/src/ICMP_Implement.c
@@ -15,6 +15,9 @@
 #include <signal.h>
 #include <time.h>
 #include <netinet/ip_icmp.h>
+#include <string.h>
+#include <unistd.h>
+
 // timeout for socket to wait for a receive command
 #define MAX_TIMEOUT = 1
 
@@ -33,6 +36,7 @@ void print_fatal_msgs(char *msg1, char *msg2);
 int char_strlen(const char *str);
 void send_ping(int socket, struct sockaddr_in address, char* to_ip, int *ttl);
 void do_interrupt(int value);
+unsigned short checksum(void *b, int len);
 
 int main(void) {
 	struct hostent *host_info;
@@ -58,7 +62,7 @@ int main(void) {
 	printf("%s has address %s\n", host_name, inet_ntoa(*distination_add));
 
 	// open a socket as row... needs super user to run because it is row socket
-	int a_row_socket = socket(PF_INET, SOCK_RAW, IPPROTO_TCP);
+	int a_row_socket = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
 	if (a_row_socket == -1) {
 		print_fatal_msg("Failed to open a raw socket");
 	}
@@ -122,6 +126,7 @@ void send_ping(int socket, struct sockaddr_in address, char* to_ip, int *ttl) {
 	struct timeval time_val;
 	time_val.tv_sec = 1;
 	time_val.tv_usec = 0;
+	int has_packet_been_sent = 1;
 	struct sockaddr_in received_address;
 
 	// times for each packet ----------->> time_start, time_end
@@ -132,14 +137,71 @@ void send_ping(int socket, struct sockaddr_in address, char* to_ip, int *ttl) {
 
 	// set timeout on the socket level to prevent it from being stuck
 	setsockopt(socket, SOL_SOCKET, SO_RCVTIMEO, (const char*) &time_val, sizeof(time_val));
-
+	struct ping_pkt icmp_packet;
 	while (to_be_interrupted) { // send echo icmp packets
-		clock_gettime(CLOCK_MONOTONIC, &time_start);
 		//prepare the icmp packet
-		clock_gettime(CLOCK_MONOTONIC, &time_end);
+		bzero(&icmp_packet, sizeof(icmp_packet));
+		icmp_packet.hdr.type = ICMP_ECHO;
+		icmp_packet.hdr.un.echo.id = getpid();
+
+		// fill the message padding part with random data
+		int i;
+		for (i = 0; i < sizeof(icmp_packet.msg)-1; i++ ) {
+			icmp_packet.msg[i]=i+'0';
+		}
+
+		icmp_packet.msg[i]='0';
+		icmp_packet.hdr.un.echo.sequence = packet_icmp_seq++;
+
+		// calculate the checksum in order to maintain the integrity of the icmp packet
+		icmp_packet.hdr.checksum = checksum(&icmp_packet, sizeof(icmp_packet));
+
+		// wait for the next echo request about half a second
+		usleep(500000);
+		clock_gettime(CLOCK_MONOTONIC, &time_start);
+		if (sendto(socket, &icmp_packet, sizeof(icmp_packet), 0, (struct sockaddr*) &address, sizeof(address)) <= 0) {
+			printf("\nPacket Sending Failed!\n");
+		    has_packet_been_sent = 0;
+		}
+
+		int received_arr_len = sizeof(received_address);
+		if (recvfrom(socket, &icmp_packet, sizeof(icmp_packet), 0, (struct sockaddr*)&received_address, &received_arr_len) <= 0 && packet_icmp_seq>1) {
+			printf("\nPacket receive failed!\n");
+		} else {
+			clock_gettime(CLOCK_MONOTONIC, &time_end);
+			double timeElapsed = ((double)(time_end.tv_nsec -  time_start.tv_nsec))/1000000.0;
+
+			if(has_packet_been_sent) {
+				if(!(icmp_packet.hdr.code == 0)) {
+					printf("Error..Packet received with ICMP type %d code %d\n", icmp_packet.hdr.type, icmp_packet.hdr.code);
+				} else {
+					int size = 64;
+					char * inet_ntoa_address = inet_ntoa(address.sin_addr);
+					printf("%d bytes from %s (%s) msg_seq=%d ttl=%d r.\n", size, inet_ntoa_address, inet_ntoa_address, packet_icmp_seq, *ttl);
+					packet_received++;
+				}
+			}
+		}
+
+
 	}
 
 
 
 	printf("hello");
+}
+
+unsigned short checksum(void *b, int len)
+{   unsigned short *buf = b;
+    unsigned int sum=0;
+    unsigned short result;
+
+    for ( sum = 0; len > 1; len -= 2 )
+        sum += *buf++;
+    if ( len == 1 )
+        sum += *(unsigned char*)buf;
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+    result = ~sum;
+    return result;
 }
